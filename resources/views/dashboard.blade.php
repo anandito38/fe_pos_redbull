3de0203e8bf7ea142771e828fadcc501b9290810
unknown <nadirhadd22@gmail.com>
2023-10-17 20:43:19 +0700

    style different bg login - dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,24 +1,22 @@
 @extends('layout')
 @section('content')
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-            <h1 class="h3 mb-4 text-gray-800">Selamat Datang di Kang Bakery</h1>
-            <div class="card mb-4 py-3 border-bottom-info">
-                <div class="card-body">
-                    {{-- @foreach ($data as $val)
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="h3 mb-4 text-gray-800">Selamat Datang di Kang Bakery</h1>
+                <div class="card mb-4 py-3 border-bottom-info">
+                    <div class="card-body">
+                        {{-- @foreach ($data as $val)
                     <p>
                         {{$val}}
                     </p>
                     @endforeach --}}
-                    <p>
-                        ANANDITOSA
-                    </p>
+                        <p>
+                            ANANDITOSA
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,14 +12,14 @@
     <title>KANG BAKERY</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
-    <link href="{{ asset('css/support-index.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/support-index.css') }}" rel="stylesheet">
 
 </head>
 
@@ -205,7 +205,8 @@
                                     Activity Log
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                <a class="dropdown-item" href="#" data-toggle="modal"
+                                    data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
                                 </a>
@@ -261,14 +262,14 @@
         </div>
 
         <!-- Bootstrap core JavaScript-->
-        <script src="{{ asset('vendor/jquery/jquery.min.js')}}"></script>
-        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
         <!-- Core plugin JavaScript-->
-        <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+        <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
         <!-- Custom scripts for all pages-->
-        <script src="{{ asset('js/sb-admin-2.min.js')}}"></script>
+        <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
 
 </body>
 
