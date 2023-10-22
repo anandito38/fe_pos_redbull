b6122d8ef2e81c4a5a5e28731c83974b4bd1b73f
nabilaurelliaa <nabilaurelliaa@gmail.com>
2023-10-22 19:16:52 +0700

    [CHORE]Adjusment model code relation v1.0

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Customers extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'fullName',
+        'nickname',
+        'password',
+        'phoneNumber',
+        'address'
+    ];
 }
---
+++ app/Models/Payment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payments extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'kode',
+        'nickname',
+        'password',
+        'phoneNumber',
+        'address',
+        'role'
+    ];
+
+    public function users(){
+        return $this->belongsTo(User::class);
+    }
+}
